cb558e891c580b7bc5aa07e9a17667b67edc4806
thomp <thompickett@gmail.com>
2016-05-16 22:48:46 -0600

    Placeholder

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -10,18 +10,18 @@ class ContractsController < ApplicationController
     #need to do some major extraction
     cart = session[:cart].transform_keys { |k| k.to_sym }
     loan_requests = LoanRequest.find(cart[:requests])
-
+    loan_offers = LoanOffer.find(cart[:offers])
     loan_requests.each do |lr|
       current_user.lent.create(loan_request_id: lr.id, borrower_id: lr.user_id)
       lr.update(active: false)
     end
 
-    loan_offers = LoanOffer.find(cart[:offers])
     loan_offers.each do |lo|
       current_user.borrowed.create(loan_offer_id: lo.id, lender_id: lo.user_id)
       lo.update(active: false)
     end
-
+    session[:cart]["requests"] = []
+    session[:cart]["offers"] = []
     redirect_to user_contracts_path(current_user.username)
   end
 
