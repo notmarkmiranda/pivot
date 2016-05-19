229d4548523e27ab4b939976180c839b067a7468
Kami Boers <kamiboers@gmail.com>
2016-05-19 09:14:32 -0600

    remove nonfunctional test code

--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -2,16 +2,10 @@ require 'rails_helper'
 
 describe ContractsController, type: :controller do
   describe "GET #new" do
-    it "returns http success" do
+    it "redirects to application page" do
       get :new
       expect(response).to have_http_status(:success)
       expect(response).to render_template "layouts/application"
     end
   end
-  describe "POST #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-   end
 end
