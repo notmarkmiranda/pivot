e73ea99291afcc14e63ce97e1f0d285da9fb0b20
Kami Boers <kamiboers@gmail.com>
2016-05-18 22:53:21 -0600

    remove binding

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -18,7 +18,6 @@ class PasswordResetsController < ApplicationController
 
   def update
     @user = User.find_by_password_reset_token!(params[:id])
-    binding.pry
     if @user.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, alert: "Password reset has expired."
     elsif @user.update(password: params[:user][:password])
